997e2a4c61765bb50731077ce09c246d53530ad7
Phantomrus <phantomrus@gmail.com>
2016-10-01 20:14:22 -0700

    First try to solve task4

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,17 @@
 import json
 
+# Теоретически, можно было сделать одной строкой, но ощущение, что читаться будет сложно.
+# print(json.dumps(json.loads(open(input("Enter the path to the file: "), 'r').read()), sort_keys=True, indent=4))
 
 def load_data(filepath):
-    pass
-
+    json_file = open(filepath, 'r')
+    json_list = json.loads(json_file.read())
+    json_file.close()
+    return json_list
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, indent=4))
 
 
-if __name__ == '__main__':
-    pass
+json_file_path = input("Enter the path to the file: ")
+pretty_print_json(load_data(json_file_path))
